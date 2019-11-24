b1276b3a7ec8e13690853bf39a37aeb07ca4302b
Onlinecheckwriter <32854124+onlinecheckwriter@users.noreply.github.com>
2019-11-24 12:40:01 -0600

    Create create-check.php

---
+++ examples/create-check.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+use onlinecheckwriter\MailCheck\Index;
+$ocw = new Index();
+$ocw->setToken("G5LoP94QISpOvk6i072yXDFBPwSjRS01foqlYPdVdYJ7li2NRkvzuHvYIzif");
+$ocw->setEnviorment("SANDBOX");
+$bank_account = $ocw->getBankAccountId(array(
+        "account_number" => 99999,
+        "routing_number" => "111111111"
+    )
+);
+$to = $ocw->createPayee(array(
+        "name" => "Alison Gambala1",
+        "address_line_1" => "95113 mark street rod",
+        "address_line_2" => "",
+        "address_city" => "Sanjose",
+        "address_state" => "CA",
+        "address_zip" => "95113",
+        "email" => "email@test.com",
+        "phone" => "900244400"
+    )
+);
+$check = $ocw->createCheck(array(
+        "amount" => 255,
+        "memo" => "First, Test check using API",
+        "note" => "",
+        "bank_account" => $bank_account['id'],
+        "to" => $to['id']
+    )
+);
+
+if ($check["completed"] == 1) {
+    print ("Successfully check created with id $check[id] ");
+} else {
+    print_r($check);
+}
