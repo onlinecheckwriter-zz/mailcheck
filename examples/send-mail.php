13f06f8897953d69e5e7914c2bd53446e7c8523a
Onlinecheckwriter <32854124+onlinecheckwriter@users.noreply.github.com>
2019-11-24 12:23:40 -0600

    Update send-mail.php

--- examples/send-mail.php
+++ examples/send-mail.php
@@ -4,14 +4,11 @@ use onlinecheckwriter\MailCheck\Index;
 
 $ocw = (new Index())->setToken("G5LoP94QISpOvk6i072yXDFBPwSjRS01foqlYPdVdYJ7li2NRkvzuHvYIzif")
     ->setEnviorment("SANDBOX");
-$bank_account = $this
-    ->ocw
-    ->getBankAccountId(array(
+$bank_account = $ocw->getBankAccountId(array(
     "account_number" => 99999,
     "routing_number" => "111111111"
 ));
-$to = $this
-    ->ocw
+$to = $ocw
     ->createPayee(array(
     "name" => "Alison Gambala1",
     "address_line_1" => "95113 mark street rod",
@@ -22,17 +19,23 @@ $to = $this
     "email" => "email@test.com",
     "phone" => "900244400"
 ));
-$check = $this
-    ->ocw
+
+$check = $ocw
     ->createCheck(array(
-    "amount" => 100,
+    "amount" => 255,
     "memo" => "First, Test check using API",
     "note" => "",
     "bank_account" => $bank_account['id'],
-    "to" => $payee['id']
+    "to" => $to['id']
 ));
-$mail = $this
-    ->ocw
+
+
+
+
+
+
+
+$mail = $ocw
     ->sentMail(array(
     "id" => $check['id'],
     "mail_type" => 3
